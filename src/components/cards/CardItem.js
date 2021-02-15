c5df8894e53868479d236e123b5174c56c704e17
José Luis López Bellido <joseluislobell@gmail.com>
2021-02-15 20:08:48 +0100

    Include popup zoom functionality in cards images
    We have use the open source React compoponent: yjose/reactjs-popup
    
    More info in: https://github.com/yjose/reactjs-popup

--- src/components/cards/CardItem.js
+++ src/components/cards/CardItem.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Button from "../button/Button";
 
+import Popup from "reactjs-popup";
+
 function CardItem({
   id,
   brand,
@@ -16,18 +18,32 @@ function CardItem({
   return (
     <>
       <li className="cards__item">
-        <Link className="cards__item__link" to={path}>
+        <div className="cards__item__link">
           <figure className="cards__item__pic-wrap" data-category={label}>
-            <img src={src} alt="SOME_IMAGE" className="cards__item__img" />
+            <Popup
+              modal
+              closeOnDocumentClick
+              trigger={
+                <img src={src} alt="SOME_IMAGE" className="cards__item__img" />
+              }
+            >
+              <img
+                src={src}
+                alt="SOME_IMAGE"
+                className="cards__item__img__popup"
+              />
+            </Popup>
           </figure>
           <div className="cards__item__info">
-            <h5 className="cards__item__text">{name}</h5>
+            <Link to={path}>
+              <h5 className="cards__item__text">{name}</h5>
+            </Link>
             <p>{description}</p>
             <Button buttonStyle="btn--secondary" buttonSize="btn--small">
               Add to cart
             </Button>
           </div>
-        </Link>
+        </div>
       </li>
     </>
   );
