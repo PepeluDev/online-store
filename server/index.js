6bf12b6b873d1ea5b4572e8d73453922880d538c
José Luis López Bellido <joseluislobell@gmail.com>
2021-03-26 14:23:26 +0100

    Fix  for heroku deployment

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.SERVER_PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 // what body parser should do
