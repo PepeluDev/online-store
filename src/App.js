30c49c84b641b8229e762cc665a083a223f9380a
José Luis López Bellido <joseluislobell@gmail.com>
2021-03-26 13:24:49 +0100

    Prepare to deploy

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
     //this.setState({ user });
     let cart = localStorage.getItem("cart");
     cart = cart ? JSON.parse(cart) : {};
-    const products = await axios.get("http://localhost:5001/products");
+    const products = await axios.get("/products");
     this.setState({ products: products.data, cart });
   }
 
